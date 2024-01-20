c686b066390c203d8fa44d71d974816bc7b91305
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-01-20 16:31:18 -0500

    Refactor homepage layout to use react-bootstrap

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,16 @@
 // import HeroImg from "../images/HeroImg.js";
 import { ReactComponent as HeroSvg } from "../images/HeroBanner.svg";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Homepage = () => {
   return (
-    <>
-      <h1 id="about">Homepage</h1>
-      <HeroSvg />
-    </>
+    <Container>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <HeroSvg className="w-100" />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
