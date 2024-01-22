66e348481c774e355a30a3e460df2ffc81299d44
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-01-22 13:40:05 -0500

    Add react-typed library and update theme colors

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -1,11 +1,43 @@
-// import React from 'react';
-import { Container } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <Container>
-      <h1>Footer</h1>
-    </Container>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col className="text-start">
+            {" "}
+            {/* Aligns content to the left */}
+            <h5>Index</h5>
+            <ul className="list-unstyled">
+              <li>About</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </Col>
+          <Col className="text-center">
+            {" "}
+            {/* Centers content horizontally */}
+            <h5>Socials</h5>
+            <ul className="list-unstyled">
+              <li>Github</li>
+              <li>LinkedIn</li>
+            </ul>
+          </Col>
+          <Col className="text-end">
+            {" "}
+            {/* Aligns content to the right */}
+            <h5>Contact Info</h5>
+            <ul className="list-unstyled">
+              <li>Fill Form</li>
+              <li>LinkedIn</li>
+              <li>Me@Mfelobes.ca</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,24 @@
 // import HeroImg from "../images/HeroImg.js";
 import { ReactComponent as HeroSvg } from "../images/HeroBanner.svg";
 import { Container, Row, Col } from "react-bootstrap";
+import Typed from "react-typed";
+import { render } from "react-dom";
 
 const Homepage = () => {
+  let x = 10;
+
   return (
     <Container>
       <Row>
         <Col md={{ span: 8, offset: 2 }}>
           <HeroSvg className="w-100" />
+          <h1 className="text-center">
+            A{" "}
+            <Typed
+              strings={["Web Developer", "UI/ UX Designer"]}
+              typeSpeed={20}
+            />
+          </h1>
         </Col>
       </Row>
     </Container>
