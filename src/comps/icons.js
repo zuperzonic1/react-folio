f1f1247c54194b19851e892ad44da3df97a832ec
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-01-28 22:20:00 -0500

    Add icons.js file with skill icons

---
+++ src/comps/icons.js
@@ -0,0 +1,36 @@
+import reactIcon from "../images/skills/react-icon@2x.png";
+import bootstrapIcon from "../images/skills/bootstrap-icon@2x.png";
+import cssIcon from "../images/skills/css-icon@2x.png";
+import htmlIcon from "../images/skills/html-icon@2x.png";
+import jsIcon from "../images/skills/js-icon@2x.png";
+import nodeIcon from "../images/skills/node-icon@2x.png";
+import psIcon from "../images/skills/ps-icon@2x.png";
+import aiIcon from "../images/skills/ai-icon@2x.png";
+import xdIcon from "../images/skills/xd-icon@2x.png";
+import wordpressIcon from "../images/skills/wordpress-icon@2x.png";
+import phpIcon from "../images/skills/php-icon@2x.png";
+import mysqlIcon from "../images/skills/mysql-icon@2x.png";
+import firebaseIcon from "../images/skills/firebase-icon@2x.png";
+import gitIcon from "../images/skills/github-icon.png";
+import threeIcon from "../images/skills/three-icon.png";
+
+// Exporting all Icons as a single object, so that we can import them in other components
+const Icons = {
+  react: reactIcon,
+  bootstrap: bootstrapIcon,
+  css: cssIcon,
+  html: htmlIcon,
+  javascript: jsIcon,
+  nodejs: nodeIcon,
+  photoshop: psIcon,
+  illustrator: aiIcon,
+  xd: xdIcon,
+  wordpress: wordpressIcon,
+  php: phpIcon,
+  mysql: mysqlIcon,
+  firebase: firebaseIcon,
+  git: gitIcon,
+  three: threeIcon,
+};
+
+export default Icons;
