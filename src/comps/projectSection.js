4aced78633aaea68c80cb7ed04dd80a942cadd86
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-02-20 17:04:01 -0500

    Add twdb1 image and update project descriptions

--- src/comps/projectSection.js
+++ src/comps/projectSection.js
@@ -13,6 +13,8 @@ import statikAi3 from "../assets/images/projects/statikAi3.png";
 import vStore1 from "../assets/images/projects/vStore1.png";
 import vStore2 from "../assets/images/projects/vStore2.png";
 
+import twdb1 from "../assets/images/projects/twdb1.png";
+
 const ProjectsSection = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -46,24 +48,26 @@ const ProjectsSection = () => {
 
   const projects = [
     {
-      title: "statikAi - Landing Page",
+      title: "Landing Website - statikAi",
       description:
-        "statkAi is a Landing page for a company that offers AI solutions for businesses. The website is built using React and Bootstrap. The website is responsive and mobile friendly.",
+        "statkAi is a Landing page for a company that offers AI solutions for businesses. I created a figma file, designed the assets with Illustrator, created a prototype for the client to check and give feedback before development. For the website I worked with React as my main framework, I then used react-router-dom to handle my site’s routing, I used Bootstrap for the general layout of the page, and used CSS for other general styling purposes.",
       demoUrl: "https://statik.mfelobes.ca/",
       technologies: [
+        { name: "Illustrator", icon: Icons.illustrator },
+        { name: "Figma", icon: Icons.figma },
         { name: "React", icon: Icons.react },
         { name: "Bootstrap", icon: Icons.bootstrap },
         { name: "Javascript", icon: Icons.javascript },
         { name: "html", icon: Icons.html },
         { name: "css", icon: Icons.css },
-        { name: "Illustrator", icon: Icons.illustrator },
       ],
       githubUrl: "https://github.com/zuperzonic1/my-site",
       images: [statikAi1, statikAi2, statikAi3],
     },
     {
-      title: "VS - THE VIRTUAL STORE",
-      description: "This is ",
+      title: "React Website - The Virtual Store",
+      description:
+        "This is a school project that I built using React and Firebase. The website is a virtual store that allows users to add items to their cart and checkout. The website is responsive and mobile friendly.",
       demoUrl: "https://vstore.mfelobes.ca/categories",
       technologies: [
         { name: "React", icon: Icons.react },
@@ -71,13 +75,13 @@ const ProjectsSection = () => {
         { name: "Bootstrap", icon: Icons.bootstrap },
         { name: "Firebase", icon: Icons.firebase },
       ],
-      githubUrl: "https://github.com/yourusername/statkik-landing-page",
+      githubUrl: "https://github.com/zuperzonic1",
       images: [vStore1, vStore2],
     },
     {
       title: "TWDB - THE WITCHER DATABASE",
       description:
-        "TWDB is a database for the Witcher universe. The website is built using Sammy.js, Foundation, PHP, MySQL, and Jquery. the website pulls data from a MySQL database and displays it on the website. The website is Desktop use for now, but will be made responsive in the future.",
+        "TWDB is a database for the Witcher TV Series. The website is built using Sammy.js, Foundation, PHP, MySQL, and Jquery. the website pulls data from a MySQL database and displays it on the website. The website is Desktop use for now, but will be made responsive in the future.",
       demoUrl: "https://staging.mfelobes.ca/mad_movies/dynamic_site",
       technologies: [
         { name: "PHP", icon: Icons.php },
@@ -85,8 +89,8 @@ const ProjectsSection = () => {
         { name: "Foundation", icon: Icons.bootstrap },
         { name: "Jquery", icon: Icons.jquery },
       ],
-      githubUrl: "https://github.com/yourusername/statkik-landing-page",
-      images: ["https://via.placeholder.com/600x400"],
+      githubUrl: "https://github.com/zuperzonic1",
+      images: [twdb1],
     },
   ];
 
@@ -96,8 +100,8 @@ const ProjectsSection = () => {
       {projects.map((project, index) => (
         <Row key={index} className="mb-5 border-project p-3 project-row">
           <Col xs={12} md={8} lg={9} className="text-md-left">
-            <h3 className="text-color mb-2">{project.title}</h3>
-            <p className="text-color">{project.description}</p>
+            <h3 className="primary-text mb-2">{project.title}</h3>
+            <p className="text-color ">{project.description}</p>
             <div className="tech-icons d-flex flex-wrap justify-content-center justify-content-md-start my-3">
               {project.technologies.map((tech, index) => (
                 <img
