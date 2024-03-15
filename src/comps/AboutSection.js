f067369d77cf44fdef4aa406fee2aab4813c7d22
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-03-14 23:38:18 -0400

    Update resume download link

--- src/comps/AboutSection.js
+++ src/comps/AboutSection.js
@@ -24,7 +24,7 @@ const AboutSection = () => {
               </button>
             </Link>
             <a
-              href="https://statik.mfelobes.ca/toDownload/resume.pdf"
+              href="https://staging.mfelobes.ca/toDownload/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
